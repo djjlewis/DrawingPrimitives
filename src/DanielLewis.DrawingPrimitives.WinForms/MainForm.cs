c6af8cf4b75017dd115c3316d67e66d10fbf98da
Dan Lewis <daniellewis@outlook.fr>
2015-08-03 16:18:46 +0200

    Renamed Form1 to MainForm

--- src/DanielLewis.DrawingPrimitives.WinForms/MainForm.cs
+++ src/DanielLewis.DrawingPrimitives.WinForms/MainForm.cs
@@ -10,13 +10,13 @@ using System.Windows.Forms;
 
 namespace DrawingPrimitives
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
 
         private PrimitiveDrawer _drawer;
 
 
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
